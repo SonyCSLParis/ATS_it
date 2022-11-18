0dda43bba4155ab507068aae1144e3a0ef6ee584
Francesca-Padovani <francesca.padovani98@gmail.com>
2022-11-18 13:33:43 +0100

    settings modification

--- settings.py
+++ settings.py
@@ -1,19 +1,15 @@
-from os import path, makedirs
-# ciao
+from os import path
 import spacy
 
 SRC_ROOT_DIR = path.dirname(path.realpath(__file__))  # path to this script
-# ROOT_DIR = path.dirname('ATS_it/')
+
 ROOT_DIR = path.dirname(SRC_ROOT_DIR)
+print(ROOT_DIR)
 DATA_DIR = path.join(ROOT_DIR + '/data')  # data in input
+
 INTERMEDIATE_DIR = path.join(ROOT_DIR, '/intermediate')  # txt and others intermediate output
+print(INTERMEDIATE_DIR)
 OUTPUT_DIR = path.join(ROOT_DIR, '/output')  # final output
-#MODEL_DIR = path.join(ROOT_DIR, '../model')  # directory for the model_deep
 HTML_DIR = path.join(ROOT_DIR, '/html_output')  # plotly html save directory for final visualisations
 
-list_dirs = [DATA_DIR, INTERMEDIATE_DIR, OUTPUT_DIR, HTML_DIR]
-
-for x in list_dirs:
-    if not path.exists(x):
-        makedirs(x)
 
