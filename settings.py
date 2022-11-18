c636ad47a08a2667017114b6690adc3cc9583132
Francesca-Padovani <francesca.padovani98@gmail.com>
2022-11-18 16:26:47 +0100

    last commit 1

--- settings.py
+++ settings.py
@@ -1,15 +1,32 @@
-from os import path
-import spacy
+from os import path,makedirs
+
 
 SRC_ROOT_DIR = path.dirname(path.realpath(__file__))  # path to this script
+SOURCE_DIR = path.join(SRC_ROOT_DIR + '/src')
+DATA_DIR = path.join(SRC_ROOT_DIR + '/data')  # data (datasets) in input
+INTERMEDIATE_DIR = path.join(SRC_ROOT_DIR + '/intermediate')  # txt and others intermediate output
+OUTPUT_DIR = path.join(SRC_ROOT_DIR + '/output')  # datasets in output
+TOKENIZER_PATH = "dbmdz/bert-base-italian-xxl-cased"
 
-ROOT_DIR = path.dirname(SRC_ROOT_DIR)
-print(ROOT_DIR)
-DATA_DIR = path.join(ROOT_DIR + '/data')  # data in input
+INCOMPLETE_DATASET_DIR = path.join(INTERMEDIATE_DIR + '/incomplete_datasets') #intermediate processing steps
+MODEL_DIR_GENERAL = path.join(SOURCE_DIR + '/model') #general model directory
+MODEL_DEEP_MARTIN = path.join(MODEL_DIR_GENERAL + '/deep_mart_final') #Deep Martin Transformer path
 
-INTERMEDIATE_DIR = path.join(ROOT_DIR, '/intermediate')  # txt and others intermediate output
-print(INTERMEDIATE_DIR)
-OUTPUT_DIR = path.join(ROOT_DIR, '/output')  # final output
-HTML_DIR = path.join(ROOT_DIR, '/html_output')  # plotly html save directory for final visualisations
+SOURCE_MODEL_DIR = path.join(MODEL_DEEP_MARTIN + '/source')
+BERT2BERT_DIR = SOURCE_MODEL_DIR + '/bert2bert'
+ANALYSIS_DIR = SOURCE_MODEL_DIR + '/analysis'
+
+TRAINED_MODEL = path.join(MODEL_DEEP_MARTIN + '/model_deep' + '/trained_model') #trained models checkpoints
+CSV_EVAL_OUTPUT = path.join(MODEL_DEEP_MARTIN + '/model_deep' + '/csv_output') #output csv after evaluation
+
+HTML_DIR = path.join(SRC_ROOT_DIR +'/html_output')  # plotly html save directory for final visualisations
+
+
+
+list_dirs = [DATA_DIR, INTERMEDIATE_DIR, OUTPUT_DIR, HTML_DIR]
+
+for x in list_dirs:
+    if not path.exists(x):
+        makedirs(x)
 
 
